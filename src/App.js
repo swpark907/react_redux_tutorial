1ea09511c54bdd02ac330d2cd13775bebc150658
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-03-02 23:08:42 +0900

    fix App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
-import { useState } from 'react';
 import './App.css';
 import AddNumberRoot from './components/AddNumberRoot';
 import DisplayNumberRoot from './components/DisplayNumberRoot';
 
 function App() {
 
-  const [number, setNumber] = useState(0);
-
   return (
     <div className="App">
       <h1>Root</h1>
-      <AddNumberRoot onClick={function(size){
-        setNumber(number + size)
-      }}></AddNumberRoot>
-      <DisplayNumberRoot number={number}></DisplayNumberRoot>
+      <AddNumberRoot></AddNumberRoot>
+      <DisplayNumberRoot ></DisplayNumberRoot>
     </div>
   );
 }
