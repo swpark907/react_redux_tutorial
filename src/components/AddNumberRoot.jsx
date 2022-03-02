46ca6769dd60158754f94cafa593ff4e0b4482db
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-03-02 23:07:08 +0900

    fix components

--- src/components/AddNumberRoot.jsx
+++ src/components/AddNumberRoot.jsx
@@ -5,9 +5,7 @@ const AddNumberRoot = (props) => {
   return (
       <div>
         <h1>Add Number Root</h1>
-        <AddNumber onClick={function(size){
-          props.onClick(size)
-        }}></AddNumber>
+        <AddNumber></AddNumber>
       </div>
   );
 };
